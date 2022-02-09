b095c1f60c9333cc2728797b38dd23eb733e2cef
Zach <zpg6@pitt.edu>
2022-02-08 19:53:01 -0500

    feat: added resolver for main repo as local dependency

--- metro.config.js
+++ metro.config.js
@@ -2,8 +2,19 @@
  * Metro configuration for React Native
  * https://github.com/facebook/react-native
  *
+ * Importing the swiftui-react-native project
+ * https://dushyant37.medium.com/how-to-import-files-from-outside-of-root-directory-with-react-native-metro-bundler-18207a348427
+ *
  * @format
  */
+
+const path = require('path');
+const extraNodeModules = {
+  common: path.resolve(__dirname + '/../swiftui-react-native/dist'),
+};
+const watchFolders = [
+  path.resolve(__dirname + '/../swiftui-react-native/dist'),
+];
 
 module.exports = {
   transformer: {
@@ -14,4 +25,14 @@ module.exports = {
       },
     }),
   },
+  resolver: {
+    extraNodeModules: new Proxy(extraNodeModules, {
+      get: (target, name) =>
+        //redirects dependencies referenced from common/ to local node_modules
+        name in target
+          ? target[name]
+          : path.join(process.cwd(), `node_modules/${name}`),
+    }),
+  },
+  watchFolders,
 };
